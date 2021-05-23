3b4789c0c4010d4698d2758258f18c650c158218
FAHRI CIGDEM <64100513+fahricigdem@users.noreply.github.com>
2021-05-23 09:54:31 +0200

    Menu and DishDetail Components are now full functional

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,35 +1,44 @@
 import React from 'react'
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
+const RenderDish = (selectedDish) => {
+    return (
+        <div className="col-12 col-md-5 m-1">
+            <Card>
+                <CardImg width="100%" src={'react-restaurant/' + selectedDish.image} alt={selectedDish.name} />
+                <CardBody>
+                    <CardTitle>{selectedDish.name}</CardTitle>
+                    <CardText>{selectedDish.description}</CardText>
+                </CardBody>
+            </Card>
+        </div>
+    )
+}
 
-const DishDetail = ({ selectedDish }) => {
-
-    const renderComments = () => {
-        return selectedDish.comments.map(comment => <li key={comment.id}>
+const RenderComments = (selectedDish) => {
+    const comments = selectedDish.comments.map(comment =>
+        <li key={comment.id}>
             <p>{comment.comment}</p>
             <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p>
-        </li>)
-    }
+        </li>
+    )
+    return (
+        <div className="col-12 col-md-5 m-1">
+            <h4>Comments</h4>
+            <ul className="list-unstyled">
+                {comments}
+            </ul>
+        </div>
+    )
+}
 
+const DishDetail = ({ selectedDish }) => {
     if (selectedDish) {
         return (
             <div className="container" >
                 <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        <Card>
-                            <CardImg width="100%" src={'react-restaurant/' + selectedDish.image} alt={selectedDish.name} />
-                            <CardBody>
-                                <CardTitle>{selectedDish.name}</CardTitle>
-                                <CardText>{selectedDish.description}</CardText>
-                            </CardBody>
-                        </Card>
-                    </div>
-                    <div className="col-12 col-md-5 m-1">
-                        <h4>Comments</h4>
-                        <ul className="list-unstyled">
-                            {renderComments()}
-                        </ul>
-                    </div>
+                    {RenderDish(selectedDish)}
+                    {RenderComments(selectedDish)}
                 </div>
             </div>
         );
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,28 +1,26 @@
 import React from 'react'
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
+const RenderMenuItem = (dish, onClick) => {
+    return (
+        <div key={dish.id} className="col-12 col-md-5 m-1">
+            <Card onClick={() => onClick(dish.id)}>
+                <CardImg width="100%" src={'react-restaurant/' + dish.image} alt={dish.name} />
+                <CardImgOverlay>
+                    <CardTitle>{dish.name}</CardTitle>
+                </CardImgOverlay>
+            </Card>
+        </div>
+    )
+}
 
 const Menu = ({ dishes, onClick }) => {
-
-    const menu = dishes.map(dish => {
-        return (
-            <div key={dish.id} className="col-12 col-md-5 m-1">
-                <Card onClick={() => onClick(dish.id)}>
-                    <CardImg width="100%" src={'react-restaurant/' + dish.image} alt={dish.name} />
-                    <CardImgOverlay>
-                        <CardTitle>{dish.name}</CardTitle>
-                    </CardImgOverlay>
-                </Card>
-            </div>
-        )
-    })
-
+    const menu = dishes.map(dish => RenderMenuItem(dish, onClick))
     return (
         <div className="container">
             <div className="row">
                 {menu}
             </div>
-
         </div>
     );
 }
