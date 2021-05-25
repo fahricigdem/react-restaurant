b2955218fa54ab8d43806927e21ca2862827a354
FAHRI CIGDEM <64100513+fahricigdem@users.noreply.github.com>
2021-05-26 00:51:55 +0200

    reducer directory deleted

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,7 +1,7 @@
-import { DISHES } from '../../shared/dishes';
-import { LEADERS } from '../../shared/leaders';
-import { PROMOTIONS } from '../../shared/promotions';
-import { COMMENTS } from '../../shared/comments';
+import { DISHES } from '../shared/dishes';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
+import { COMMENTS } from '../shared/comments';
 
 export const initialState = {
     dishes: DISHES,
